b9790408829c442ba5231a45b0f475cbf4a2734b
IWsz1 <soraa24926@gmail.com>
2023-11-01 16:03:53 +0900

    チャットルーム作成機能

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,5 +3,17 @@ class RoomsController < ApplicationController
     @room = Room.new
   end
   def create
+    # binding.pry
+    if Room.create(room_params)
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  private
+
+  def room_params
+    # user_idsというカラムはroomに存在しないが、中間テーブルのuser_idに一要素ずつ保存される
+    params.require(:room).permit(:name, user_ids: [])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-   validates :name,presence:true, length:{maximum:6}
+  validates :name,presence:true
+
+  has_many :room_users
+  has_many :rooms, through: :room_users
 end
