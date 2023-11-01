f2a94ba2f58f509fa95ffa312ac838759ab31906
IWsz1 <soraa24926@gmail.com>
2023-11-01 12:37:21 +0900

    ユーザー認証機能の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  # ログインしていなければログイン画面に移動
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  # deviseへ新たに追加するメソッドをconfigure_permitted_parametersというメソッドで作ることが慣習
+  def configure_permitted_parameters
+    # deviseの受け取れるパラメーターの宣言に新しいパラメーターを追加するメソッド
+    # 第一引数にサインインサインアップアカウント情報の更新のどれかを記載　第三引数に追加したいカラム名のキーを記述する
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
