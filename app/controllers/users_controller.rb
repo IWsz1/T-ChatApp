6d0be339181cd51560c5d423403bdcd2739a4420
IWsz1 <soraa24926@gmail.com>
2023-11-01 13:17:10 +0900

    ログイン情報の変更機能

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  def edit
+    # binding.pry
+  end
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  def user_params
+    # paramsハッシュは色んな情報が入っているため、取得する情報を限定するために、requireでフォーム記入欄の指定カラムと紐づけたモデル名を、permitではフォームの中の情報から更に必要な入力欄を絞るために、フォームで紐づけたカラム名を記載する
+    # mergeで他データベースの値をテーブルへ追加するデータに含めることができる
+    # current_userでログイン中のレコードを抽出
+    params.require(:user).permit(:name,:email)
+  end
+end
