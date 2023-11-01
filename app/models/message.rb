ff0df74c23f0d6b3dbafd792172200d9e5659845
IWsz1 <soraa24926@gmail.com>
2023-11-01 22:05:04 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,9 @@ class Message < ApplicationRecord
   belongs_to :room
 
   validates :content,presence:true
+  # has_one_attachedで1つのレコードに1つのファイル(image)を添付できる
+  # ファイル名がモデルと紐づいたフォームから送られてくるフォルダのキーになる
+  # この仕組みでmessageレコードに画像を添付する
+  # カラムの設定は必要ない
+  has_one_attached :image
 end
