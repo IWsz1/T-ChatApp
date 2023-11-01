b5135d447fe78a82d35f8dd986544ce917990050
IWsz1 <soraa24926@gmail.com>
2023-11-01 20:54:20 +0900

    修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
-    @messages = Message.includes(:user)
+    @messages = @room.messages.includes(:user)
   end
   def create
     # カレントルームのレコードを取得
@@ -12,7 +12,7 @@ class MessagesController < ApplicationController
       redirect_to room_messages_path(@room)
     else
       # renderで再出力を行う際に@messagesのデータは送信ボタンを押した際に失われているため再度ビューへ渡す
-      @messages = Message.includes(:user)
+      @messages = @room.messages.includes(:user)
       render :index, status: :unprocessable_entity
     end
   end
